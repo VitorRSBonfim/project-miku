207f2a1d5a4349be9fb5d82c7be9de9b01564423
Vitor Rodrigo <120431996+VitorRSBonfim@users.noreply.github.com>
2026-04-10 13:13:23 +0000

    __fastbackup

--- data/db/data.py
+++ data/db/data.py
@@ -1,8 +1,12 @@
 # wallet
 
-wallet = {'date' : '10/10/2026', 'price' : '20.50', 'currency' : 'USD', 'Ctype' : 'BTC', 'Ctpye_ammout' : 0.00028148}
-[]
+# catching the price by 1x1 (1USDC = 0.0000137253 BTC)
+
+# Ask
+# price = input("1x1 BTC/USDC price")
+price = 
 
 
- 
+wallet = {'date' : '2026-04-08', 'price' : price, 'currency' : 'USD', 'Ctype' : 'BTC', 'Ctpye_ammout' : 0.00028148}
+
 

78dec02079f68f045ffbeb2e2b72316e9f2fe4fa
Vitor Rodrigo <120431996+VitorRSBonfim@users.noreply.github.com>
2026-04-18 12:51:02 +0000

    __wallet_apreciation ok

--- services/usdc_btc/main.py
+++ services/usdc_btc/main.py
@@ -12,10 +12,10 @@ import json
 # Calc 
 # % = ((Valor final - Valor incial) / valor incial) * 100
 
-btc_usc = 0.0000137253 # preço no momento da compra 1 / BTC
-btc_price = 1 / (btc_usc)
+#btc_usc = 0.0000137253 # preço no momento da compra 1 / BTC
+#btc_price = 1 / (btc_usc)
 # print(btc_price)
-usdcAmmount = 20.50870926
+usdcAmmount = 0
 
 wallet = [{'date' : '2026-04-08', 'price' : 1 / (0.0000137253), 'currency' : 'USD', 'usdcAmmount' : 20.50870926,'cType' : 'BTC', 'cAmmout' : 0.00028148, 'initialValue' : 0.00028148 * 72858}, {'date' : '2026-04-15', 'price' : 1 / (0.0000134623), 'currency' : 'USD', 'usdcAmmount' : 26.42138372,'cType' : 'BTC', 'cAmmout' : 0.00035569, 'initialValue' : 0.00035569 * 74281}]
 
@@ -34,31 +34,28 @@ print("COTAÇÃO BTC COMPRA EM USDC", (data['BTCUSD']['bid']))
 passed = float(data['BTCUSD']['bid'])
 intBTC = int(passed)
 
-
-
-
-finalCurrency = wallet[0]['cAmmout'] * intBTC
-initialCurrency = wallet[0]['initialValue']
-appreciation = ((finalCurrency - initialCurrency) / initialCurrency) * 100
-final = initialCurrency + ((appreciation / 100)) * initialCurrency
-lucro = ((finalCurrency - initialCurrency))
+appreciation = 0
+lucro = 0
 total_invest = 0
-count_ = 0
 
 for c in wallet:
-    print(wallet[count_])
-    print(wallet[count_]['date'])
-    total_invest += wallet[count_]['usdcAmmount']
-    count_ += 1
-    print(f"DDDDDDDDD{total_invest:.2f}")
+    lenght_wallet = len(wallet)
+    # Capturando valor investido individualmente com loop for
+    uniqueUSDC = c['usdcAmmount']
+    # inicial - atual
+    total_invest += c['usdcAmmount']
+    initialCurrency = c['initialValue']
+    finalCurrency = c['cAmmout'] * intBTC
+    # calculo entre total investido e % de valorização / devalorização 
+    print(f"USDC {finalCurrency:.2f}, {uniqueUSDC:2f}")
+    lucro += (finalCurrency - uniqueUSDC) 
+    appreciation = (lucro / total_invest) * 100
+    print(f"TOTAL INVEST DE {total_invest:.2f}")
+    print(f"LUCRO DE {lucro:.2f}")
+    print(f"VALORIZAÇÃO DE {appreciation:.2f}% VALOR FINAL NA CARTEIRA DE {((total_invest * (appreciation / 100)) + total_invest):.2f}")
+    
 
-# calculating the wallet variation
-
-atual_ = wallet
-# variation =
 
 # print(wallet[0]['date'])
 # print(f"LENG DE DADOS CARTEIRA LOOP FOR {len(wallet)}")
 
-btc = wallet[0]['cAmmout'] * intBTC
-print(f"{finalCurrency:.2f} {initialCurrency:.2f} {appreciation} {final:.2f} {lucro}")
